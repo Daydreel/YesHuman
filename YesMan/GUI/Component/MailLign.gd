7e16cf275fa9923ea45d9550efb18c748f630e86
Daydreel <daydreel@gmail.com>
2021-12-05 10:47:26 +0100

    assets + screen mailbox + screen transition + some more code

---
+++ YesMan/GUI/Component/MailLign.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _on_MailLign_pressed():
+	print(self, " I have been pressed")
+	pass # Replace with function body.
---
+++ YesMan/QuitButton.gd
@@ -0,0 +1,5 @@
+extends TextureButton
+
+
+func _on_QuitButton_pressed():
+	get_tree().quit()
