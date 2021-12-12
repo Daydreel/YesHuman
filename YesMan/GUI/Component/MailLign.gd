7efc8d7f1d8f1d00caabcba5c6179642f34f2021
JK <daydreel@gmail.com>
2021-12-12 14:31:17 +0100

    added mail data + parser + mail list functionnality

--- YesMan/GUI/Component/MailLign.gd
+++ YesMan/GUI/Component/MailLign.gd
@@ -1,5 +1,18 @@
 extends Button
 
+var expediteur = ""
+var objet_mail = ""
+var date_reception = ""
+
+onready var expediteur_lbl = $MarginContainer/HBoxContainer/FromLabel
+onready var objet_lbl = $MarginContainer/HBoxContainer/SubjectLabel
+onready var date_lbl = $MarginContainer/HBoxContainer/DateLabel
+
+
+func _ready():
+	expediteur_lbl.text = expediteur
+	objet_lbl.text = objet_mail
+	date_lbl.text = date_reception
+
 func _on_MailLign_pressed():
 	print(self, " I have been pressed")
-	pass # Replace with function body.
--- YesMan/Scenes/BoiteMail/BoiteMail.gd
+++ YesMan/Scenes/BoiteMail/BoiteMail.gd
@@ -1,6 +1,28 @@
 extends Control
 
+export(String, FILE, "*.json") var mails_data_path
+var _mail_lign_path = preload("res://GUI/Component/MailLign.tscn")
+onready var _mail_list = $MailList
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var mails = _load_data_mails_raw()
+	for mail in mails:
+		instance_mail_lign(mail["Expediteur"],mail["ObjetMail"], mail["DateReception"])
+
+
+func _load_data_mails_raw():
+	var file = File.new()
+	file.open(mails_data_path, File.READ)
+	var json = JSON.parse(file.get_as_text())
+	file.close()
+	var content = json.result
+	return content
+
+func instance_mail_lign(var expediteur = "", var objet_mail = "", var date_reception = ""):
+	var mail_lign = _mail_lign_path.instance()
+	mail_lign.expediteur = expediteur
+	mail_lign.objet_mail = objet_mail
+	mail_lign.date_reception = date_reception
+	_mail_list.add_child(mail_lign)
+	
